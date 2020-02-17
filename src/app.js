8a40933bac4bcec43f21396565a15ab67ae4cd63
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-02-17 11:10:32 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-//Trial 1 - create a Manager array and return it
+//Progression 1 - create a Manager array and return it
 let managerName = "Alex Ferguson";
 let managerAge = 78;
 let currentTeam = "Manchester FC";
@@ -18,7 +18,7 @@ try {
   // do nothing - expected error
 }
 
-//Trial 2 - create a formation object and return it
+//Progression 2 - create a formation object and return it
 var formation = [4, 4, 3];
 
 //write your function here
@@ -31,23 +31,23 @@ try {
   //do nothing
 }
 
-//Trial 3 - Filter players that debuted in ___ year
+//Progression 3 - Filter players that debuted in ___ year
 
-//Trial 4 - Filter players that play at the position _______
+//Progression 4 - Filter players that play at the position _______
 
-//Trial 5 - Filter players that have won ______ award
+//Progression 5 - Filter players that have won ______ award
 
-//Trial 6 - Filter players that won ______ award ____ times
+//Progression 6 - Filter players that won ______ award ____ times
 
-//Trial 7 - Filter players that won ______ award and belong to ______ country
+//Progression 7 - Filter players that won ______ award and belong to ______ country
 
-//Trial 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
+//Progression 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
 
-//Challenge 1 - Sort players in descending order of their age
+//Progression 9 - Sort players in descending order of their age
 
-//Challenge 2 - Sort players beloging to _____ team in descending order of awards won
+//Progression 10 - Sort players beloging to _____ team in descending order of awards won
 
-//Judgement 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
+//Challenge 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
 
-//Judgement 2 - Sort players that are older than _____ years in alphabetical order
+//Challenge 2 - Sort players that are older than _____ years in alphabetical order
 //Sort the awards won by them in reverse chronological order
